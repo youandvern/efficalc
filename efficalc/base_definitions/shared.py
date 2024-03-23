687bdad8cf3638bb40d684eee9883ba412e0f5f2
youandvern <ayounguw@gmail.com>
2024-03-23 16:34:03 -0700

    Add thread-safety for calculations

--- efficalc/base_definitions/shared.py
+++ efficalc/base_definitions/shared.py
@@ -1,3 +1,5 @@
+import threading
+
 from latexexpr_efficalc import Expression, Operation, Variable
 
 OPERATOR_TO_LATEX = {
@@ -12,7 +14,7 @@ OPERATOR_TO_LATEX = {
 
 _DEFAULT_OVERRIDE_KEY = "default_overrides"
 _ALL_CALC_ITEMS_KEY = "all_calc_items"
-_GLOBAL_STORE = {_DEFAULT_OVERRIDE_KEY: {}, _ALL_CALC_ITEMS_KEY: []}
+_THREAD_LOCAL_STORE = threading.local()
 
 
 class CalculationItem(object):
@@ -21,20 +23,31 @@ class CalculationItem(object):
     super_type = "CalculationItem"
 
 
+def _get_thread_local_store():
+
+    if not hasattr(_THREAD_LOCAL_STORE, "efficalc_store"):
+        _THREAD_LOCAL_STORE.efficalc_store = {
+            _DEFAULT_OVERRIDE_KEY: {},
+            _ALL_CALC_ITEMS_KEY: [],
+        }
+
+    return _THREAD_LOCAL_STORE.efficalc_store
+
+
 def save_calculation_item(item):
     """Save an item to the global store of all calculation items."""
-    _GLOBAL_STORE[_ALL_CALC_ITEMS_KEY].append(item)
+    _get_thread_local_store()[_ALL_CALC_ITEMS_KEY].append(item)
 
 
 def clear_saved_objects():
     """Clear all saved calculation items from the global store."""
-    _GLOBAL_STORE[_ALL_CALC_ITEMS_KEY] = []
+    _get_thread_local_store()[_ALL_CALC_ITEMS_KEY] = []
 
 
 def get_override_or_default_value(input_name: str, default_value: any):
     """Get the default override value for a given input name from the global store. If no override is found, returns
     the default value."""
-    default_overrides: dict = _GLOBAL_STORE[_DEFAULT_OVERRIDE_KEY]
+    default_overrides: dict = _get_thread_local_store()[_DEFAULT_OVERRIDE_KEY]
     if default_overrides is not None and input_name in default_overrides:
         return default_overrides.get(input_name)
     else:
@@ -43,17 +56,17 @@ def get_override_or_default_value(input_name: str, default_value: any):
 
 def set_input_default_overrides(default_overrides: dict[str, any]):
     """Set default override values for input names in the global store."""
-    _GLOBAL_STORE[_DEFAULT_OVERRIDE_KEY] = default_overrides
+    _get_thread_local_store()[_DEFAULT_OVERRIDE_KEY] = default_overrides
 
 
 def get_all_calc_objects() -> list:
     """Get all calculation objects saved in the global store."""
-    return _GLOBAL_STORE[_ALL_CALC_ITEMS_KEY]
+    return _get_thread_local_store()[_ALL_CALC_ITEMS_KEY]
 
 
 def clear_all_input_default_overrides():
     """Clear all input default overrides from the global store."""
-    _GLOBAL_STORE[_DEFAULT_OVERRIDE_KEY] = {}
+    _get_thread_local_store()[_DEFAULT_OVERRIDE_KEY] = {}
 
 
 def _get_float_safe_operation(
