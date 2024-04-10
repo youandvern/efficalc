2f6126351127d0e84533f068f147925fa00d1b7d
youandvern <ayounguw@gmail.com>
2024-04-09 20:52:53 -0700

    Add aluminum section properties

--- tests/sections/test_section_query.py
+++ tests/sections/test_section_query.py
@@ -8,6 +8,11 @@ from efficalc.sections.section_query import (
     get_aisc_rectangular,
     get_aisc_tee,
     get_aisc_wide_flange,
+    get_aluminum_angle,
+    get_aluminum_channel,
+    get_aluminum_circular,
+    get_aluminum_rectangular,
+    get_aluminum_wide_flange,
 )
 
 
@@ -99,4 +104,77 @@ def test_wide_flange_lookup():
 def test_wide_flange_lookup_size_not_found():
     with pytest.raises(ValueError) as e_info:
         get_aisc_wide_flange("NOT_FOUND")
+    assert "section size named NOT_FOUND could not be found" in str(e_info.value)
+
+
+def test_aluminum_angle_lookup():
+    section = get_aluminum_angle("L 3 x 2 x 3/8")
+    assert section.d == 3
+    assert section.b == 2
+    assert section.t == 0.375
+    assert section.A == 1.74
+    assert section.W == 2.05
+
+
+def test_aluminum_angle_lookup_size_not_found():
+    with pytest.raises(ValueError) as e_info:
+        get_aluminum_angle("NOT_FOUND")
+    assert "section size named NOT_FOUND could not be found" in str(e_info.value)
+
+
+def test_aluminum_channel_lookup():
+    section = get_aluminum_channel("C 4 X 2.50")
+    assert section.d == 4
+    assert section.W == 2.50
+    assert section.A == 2.13
+    assert section.b == 1.72
+
+
+def test_aluminum_channel_lookup_size_not_found():
+    with pytest.raises(ValueError) as e_info:
+        get_aluminum_channel("NOT_FOUND")
+    assert "section size named NOT_FOUND could not be found" in str(e_info.value)
+
+
+def test_aluminum_circular_lookup():
+    section = get_aluminum_circular("HSS 6 x 0.25")
+    assert section.OD == 6
+    assert section.t == 0.25
+    assert section.ID == 5.5
+    assert section.W == 5.31
+    assert section.A == 4.52
+
+
+def test_aluminum_circular_lookup_size_not_found():
+    with pytest.raises(ValueError) as e_info:
+        get_aluminum_circular("NOT_FOUND")
+    assert "section size named NOT_FOUND could not be found" in str(e_info.value)
+
+
+def test_aluminum_rectangular_lookup():
+    section = get_aluminum_rectangular("RT 6 x 2 x 1/4")
+    assert section.d == 6
+    assert section.b == 2
+    assert section.t == 0.25
+    assert section.W == 4.41
+    assert section.A == 3.75
+
+
+def test_aluminum_rectangular_lookup_size_not_found():
+    with pytest.raises(ValueError) as e_info:
+        get_aluminum_rectangular("NOT_FOUND")
+    assert "section size named NOT_FOUND could not be found" in str(e_info.value)
+
+
+def test_aluminum_wide_flange_lookup():
+    section = get_aluminum_wide_flange("WF 6 x 8.30")
+    assert section.d == 6
+    assert section.W == 8.30
+    assert section.b == 6
+    assert section.A == 7.06
+
+
+def test_aluminum_wide_flange_lookup_size_not_found():
+    with pytest.raises(ValueError) as e_info:
+        get_aluminum_wide_flange("NOT_FOUND")
     assert "section size named NOT_FOUND could not be found" in str(e_info.value)
--- utils/build_section_tables.py
+++ utils/build_section_tables.py
@@ -1,5 +1,5 @@
 """
-This script can be used to build the sections database that is packaged with the library. It reads the individual CSV
+This script creates a clean build of the sections database packaged with the library. It reads the individual CSV
 files for each section type and writes them to a table in the sections database with an index on the AISC_name column.
 """
 
@@ -17,10 +17,20 @@ from efficalc.sections.section_query import (
     AISC_RECTANGULAR_TABLE,
     AISC_TEE_TABLE,
     AISC_WIDE_FLANGE_TABLE,
+    ALUM_ANGLE_TABLE,
+    ALUM_CHANNEL_TABLE,
+    ALUM_CIRCULAR_TABLE,
+    ALUM_RECTANGULAR_TABLE,
+    ALUM_SECTION_SIZE_NAME_COLUMN,
+    ALUM_WIDE_FLANGE_TABLE,
     SECTIONS_DB_NAME,
 )
 
-if __name__ == "__main__":
+
+def clean_build_section_tables():
+
+    print("Building section tables...")
+
     conn = sqlite3.connect(f"efficalc/sections/{SECTIONS_DB_NAME}")
 
     df = pd.read_csv("efficalc/sections/csv/aisc_sections_2L.csv")
@@ -65,4 +75,40 @@ if __name__ == "__main__":
         f"CREATE UNIQUE INDEX unique_aisc_name_{AISC_WIDE_FLANGE_TABLE} ON {AISC_WIDE_FLANGE_TABLE}(AISC_name);"
     )
 
+    df = pd.read_csv("efficalc/sections/csv/alum_shapes_channel.csv")
+    df.to_sql(ALUM_CHANNEL_TABLE, conn, if_exists="replace", index=False)
+    conn.execute(
+        f"CREATE UNIQUE INDEX unique_alum_name_{ALUM_CHANNEL_TABLE} ON {ALUM_CHANNEL_TABLE}({ALUM_SECTION_SIZE_NAME_COLUMN});"
+    )
+
+    df = pd.read_csv("efficalc/sections/csv/alum_shapes_circular.csv")
+    df.to_sql(ALUM_CIRCULAR_TABLE, conn, if_exists="replace", index=False)
+    conn.execute(
+        f"CREATE UNIQUE INDEX unique_alum_name_{ALUM_CIRCULAR_TABLE} ON {ALUM_CIRCULAR_TABLE}({ALUM_SECTION_SIZE_NAME_COLUMN});"
+    )
+
+    df = pd.read_csv("efficalc/sections/csv/alum_shapes_L.csv")
+    df.to_sql(ALUM_ANGLE_TABLE, conn, if_exists="replace", index=False)
+    conn.execute(
+        f"CREATE UNIQUE INDEX unique_alum_name_{ALUM_ANGLE_TABLE} ON {ALUM_ANGLE_TABLE}({ALUM_SECTION_SIZE_NAME_COLUMN});"
+    )
+
+    df = pd.read_csv("efficalc/sections/csv/alum_shapes_rectangular.csv")
+    df.to_sql(ALUM_RECTANGULAR_TABLE, conn, if_exists="replace", index=False)
+    conn.execute(
+        f"CREATE UNIQUE INDEX unique_alum_name_{ALUM_RECTANGULAR_TABLE} ON {ALUM_RECTANGULAR_TABLE}({ALUM_SECTION_SIZE_NAME_COLUMN});"
+    )
+
+    df = pd.read_csv("efficalc/sections/csv/alum_shapes_wf.csv")
+    df.to_sql(ALUM_WIDE_FLANGE_TABLE, conn, if_exists="replace", index=False)
+    conn.execute(
+        f"CREATE UNIQUE INDEX unique_alum_name_{ALUM_WIDE_FLANGE_TABLE} ON {ALUM_WIDE_FLANGE_TABLE}({ALUM_SECTION_SIZE_NAME_COLUMN});"
+    )
+
     conn.close()
+
+    print("Section tables built successfully.")
+
+
+if __name__ == "__main__":
+    clean_build_section_tables()
